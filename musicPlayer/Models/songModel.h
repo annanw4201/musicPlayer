4fa88dc6d8d0b66b969354d4cffac1888a905273
Wong Tom <23562997+tomwang1024@users.noreply.github.com>
2019-01-13 21:02:47 -0500

    change some git ignore files

---
+++ musicPlayer/Models/songModel.h
@@ -0,0 +1,21 @@
+//
+//  songModel.h
+//  musicPlayer
+//
+//  Created by Wong Tom on 2018-08-30.
+//  Copyright © 2018 Wang Tom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class UIImage;
+@class lrcModel;
+
+@interface songModel : NSObject
+@property (nonatomic, copy) NSString *songmid;
+@property (nonatomic, copy) NSString *songName;
+@property (nonatomic, copy) NSString *singer;
+@property (nonatomic, copy) NSString *songAlbumName;
+@property (nonatomic, copy) UIImage *songMPArtWork;
+@property (nonatomic, copy) NSURL *songURL;
+@property (nonatomic, strong)lrcModel *lrcModel;
+@end
